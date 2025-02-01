eb2d3040de747fb87194bb398f6678bc1a5ddf9d
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2025-02-01 03:57:26 -0800

    update media file directory path to point to new location

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ struct MediaFile {
 }
 
 fn main() {
-    let dir_path = "/media/pinas/foo1/Music/Music/D".to_string();
+    let dir_path = "/media/pinas/foo1/Music/Music/E".to_string();
 
     let media_files = find_media(&dir_path);
 
